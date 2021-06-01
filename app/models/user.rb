70b6010d88917bc444416a22fa8ebba16af464dd
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-05-31 23:11:15 -0700

    configure auth pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30, case_sensitive: false }
+
+def email_required?
+  false
+end
+
+def password_required?
+  false
+end
 end
--- db/migrate/20210601055016_addname_to_user.rb
+++ db/migrate/20210601055016_addname_to_user.rb
@@ -2,4 +2,12 @@ class AddnameToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
   end
+
+  def email_required?
+    false
+  end
+
+  def password_required?
+    false
+  end
 end
