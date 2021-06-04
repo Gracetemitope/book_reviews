4ace6abaa433dca02d4983d55281a04b154df0e5
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-06-03 23:05:40 -0700

    create category scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_04_055612) do
+ActiveRecord::Schema.define(version: 2021_06_04_060215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer "author_id"
