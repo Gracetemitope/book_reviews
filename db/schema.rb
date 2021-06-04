b7730a3467015783492bf63b50a1da1d3378bc0a
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-06-03 23:01:03 -0700

    create voting scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_063802) do
+ActiveRecord::Schema.define(version: 2021_06_04_055612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,13 @@ ActiveRecord::Schema.define(version: 2021_06_01_063802) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "review_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "votes", "reviews"
+  add_foreign_key "votes", "users"
 end
