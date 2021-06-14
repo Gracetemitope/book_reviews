81ef0d9c45049e1852c6d45bad37fa09a9301161
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-06-14 00:54:21 -0700

    fix linters

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,44 +1,44 @@
 require 'test_helper'
 
-class reviewsControllerTest < ActionDispatch::IntegrationTest
+class ReviewsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @review = reviews(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get reviews_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_article_url
     assert_response :success
   end
 
-  test "should create article" do
+  test 'should create article' do
     assert_difference('Article.count') do
-      post reviews_url, params: { article: {  } }
+      post reviews_url, params: { article: {} }
     end
 
     assert_redirected_to article_url(Article.last)
   end
 
-  test "should show article" do
+  test 'should show article' do
     get article_url(@review)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_article_url(@review)
     assert_response :success
   end
 
-  test "should update article" do
-    patch article_url(@review), params: { article: {  } }
+  test 'should update article' do
+    patch article_url(@review), params: { article: {} }
     assert_redirected_to article_url(@review)
   end
 
-  test "should destroy article" do
+  test 'should destroy article' do
     assert_difference('Article.count', -1) do
       delete article_url(@review)
     end
