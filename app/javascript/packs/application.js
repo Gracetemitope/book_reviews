3da6a86571e2d8f3a160190411c7321421422d82
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-06-17 10:01:55 -0700

    --hotfix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,28 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require owl.carousel
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
 
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 import "bootstrap"
-// import "../stylesheets/application";
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel';
-import JQuery from 'jquery';
-window.$ = window.JQuery = JQuery;
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// $(document).ready(function(){ 
-//   $('.flexslider').flexslider();
-// });
+import "@fortawesome/fontawesome-free/css/all.css"
