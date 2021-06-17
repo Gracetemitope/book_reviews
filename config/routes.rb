e9067b5061faed551644d1325c5df315c3044251
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-06-16 19:17:46 -0700

    work on reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
   root 'categories#index'
 
   devise_for :users
-  #   get '/users/sign_out' => 'devise/sessions#destroy'
-  # end
 
   resources :reviews 
-  # resources :votes
   get 'vote/:id', to: 'votes#vote'
 
-  # end
-  # resources :votes
+  
   resources :categories, only: %i[index show new create edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
